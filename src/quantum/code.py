07e6e56b5d5f9f340d18c2b24812f7170c5f7515
Bao Nguyen <bao.nguyen@u.nus.edu>
2026-08-17 23:44:13 +0800

    Validation of cudaq installation

---
+++ src/quantum/code.py
@@ -0,0 +1,22 @@
+import sys
+import cudaq
+
+print(f"Running on targer {cudaq.get_target().name}")
+qubit_count = int(sys.argv[1]) if 1 < len(sys.argv) else 2
+
+@cudaq.kernel
+def kernel():
+    qubits = cudaq.qvector(qubit_count)
+    h(qubits[0])
+    for i in range(1, qubit_count):
+        x.ctrl(qubits[0], qubits[i])
+    mz(qubits)
+
+def main() -> None:
+    result = cudaq.sample(kernel)
+    print(f"Result = {result}");
+    print("Succefully")
+
+
+if __name__ == "__main__":
+    main()
